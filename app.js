fe00369933f00f501da5cb9d6a392ef97754d1cc
olumayor99 <32824520+olumayor99@users.noreply.github.com>
2020-07-01 14:53:59 +0300

    added middleware

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require("express");
 
 const app = express();
 
+//Middleware
+app.use("/posts", () => {
+  console.log("This is a middleware running");
+});
+
 //ROUTES
 app.get("/", (req, res) => {
-  res.send("We are home");
+  res.send("We are on home");
+});
+
+app.get("/posts", (req, res) => {
+  res.send("We are on posts");
 });
 
 //LISTENING
